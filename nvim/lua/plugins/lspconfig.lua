538010921d6961c6cfd6cf98eecacf2bee01b09a
Craig Butcher <hello@craigbutcher.net>
2025-03-16 00:20:46 +0000

    Update HTML to prevent formatting of the following tag - pre, code, textarea

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -189,7 +189,16 @@ return {
 					-- But for many setups, the LSP (`ts_ls`) will work just fine
 					-- ts_ls = {},
 					--
-
+					--
+					html = {
+						settings = {
+							html = {
+								format = {
+									contentUnformatted = "pre,code,textarea",
+								},
+							},
+						},
+					},
 					lua_ls = {
 						-- cmd = {...},
 						-- filetypes = { ...},
