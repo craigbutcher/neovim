343e24e053b163374c3610f4750b96176b8c729d
Craig Butcher <hello@craigbutcher.net>
2025-12-02 22:33:35 +0000

    conform plugin fix

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -42,54 +42,28 @@ return {
         stop_after_first = true,
       },
     },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
-        css = { 'prettier' },
-        json = { 'prettier' },
+
+    -- Custom formatter definitions
+    formatters = {
+      jsonrepair = {
+        command = 'jsonrepair',
+        stdin = true,
+        args = {},
       },
-      -- Custom formatter definitions
-      formatters = {
-        jsonrepair = {
-          command = 'jsonrepair',
-          stdin = true,
-          args = {},
+      jq = {
+        command = 'jq',
+        args = { '.' },
+        stdin = true,
+      },
+      stylelint = {
+        command = 'stylelint',
+        args = {
+          '--fix',
+          '--stdin',
+          '--stdin-filename',
+          '$FILENAME',
         },
-        jq = {
-          command = 'jq',
-          args = { '.' },
-          stdin = true,
-        },
-        stylelint = {
-          command = 'stylelint',
-          args = {
-            '--fix',
-            '--stdin',
-            '--stdin-filename',
-            '$FILENAME',
-          },
-          stdin = true,
-        },
+        stdin = true,
       },
     },
   },
